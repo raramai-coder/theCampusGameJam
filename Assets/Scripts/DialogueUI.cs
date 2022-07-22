776604ccd22ed78bc953e128964e51c767c1126a
Matipa Marufu <2344503@students.wits.ac.za>
2022-07-22 20:31:05 +0200

    Added UI to dialogue

---
+++ Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUI : MonoBehaviour
+{
+    PlayerDialogue PlayerDialogue;
+    GameObject ChoosePlayerPanel;
+    GameObject PanelQ1;
+
+    [SerializeField]
+    Button JamesBtn;
+    [SerializeField]
+    Button ThandiBtn;
+    [SerializeField]
+    Button KaliBtn;
+    [SerializeField]
+    Button RoliBtn;
+    [SerializeField]
+    Button BandileBtn;
+
+    //tracks number of times a person was called
+    public int calledJames = 0;
+    public int calledThandi = 0;
+    public int calledKali = 0;
+    public int calledRoli = 0;
+    public int calledBandile = 0;
+
+//tracks which person the player has called
+    public bool callJames = false;
+    public bool callThandi = false;
+    public bool callKali = false;
+    public bool callRoli = false;
+    public bool callBandile = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerDialogue = GameObject.FindGameObjectWithTag("Canvas").GetComponent<PlayerDialogue>();
+        ChoosePlayerPanel= GameObject.FindGameObjectWithTag("PlayerPanel");
+        PanelQ1= GameObject.FindGameObjectWithTag("PanelQ1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(calledKali >= 2)
+        {
+            KaliBtn.interactable = false;
+        }
+        else if(calledBandile >= 2)
+        {
+            BandileBtn.interactable = false;
+        }
+          else if(calledJames >= 2)
+        {
+            JamesBtn.interactable = false;
+        }
+          else if(calledThandi >= 2)
+        {
+            ThandiBtn.interactable = false;
+        }
+          else if(calledRoli >= 2)
+        {
+            RoliBtn.interactable = false;
+        }
+    }
+
+    public void callJamesOption()
+    {
+        callJames = true;
+        openNextPanel();
+    }
+
+      public void callThandiOption()
+    {
+        callThandi = true;
+        openNextPanel();
+    }
+
+      public void callKaliOption()
+    {
+        callKali = true;
+        openNextPanel();
+    }
+
+      public void callRoliOption()
+    {
+        callRoli = true;
+        openNextPanel();
+    }
+
+      public void callBandileOption()
+    {
+        callBandile = true;
+        calledBandile += 1;
+        openNextPanel();
+    }
+
+    void openNextPanel()
+    {
+        ChoosePlayerPanel.SetActive(false);
+        PanelQ1.SetActive(true);
+    }
+}
--- Assets/Scripts/PlayerDialogue.cs
+++ Assets/Scripts/PlayerDialogue.cs
@@ -1,35 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerDialogue : MonoBehaviour
 {
     List<string> NPCs = new List<string>();
-    Dictionary<int, string> PlayerGeneralQuestions = new Dictionary<int, string>();
+    public Dictionary<int, string> PlayerGeneralQuestions = new Dictionary<int, string>();
 
+//Personal Questions Dictionary
     Dictionary<int, string> KalushiQuestions = new Dictionary<int, string>();
     Dictionary<int, string> BandileQuestions = new Dictionary<int, string>();
     Dictionary<int, string> JamesQuestions = new Dictionary<int, string>();
     Dictionary<int, string> ThandiQuestions = new Dictionary<int, string>();
     Dictionary<int, string> RoliQuestions = new Dictionary<int, string>();
 
+//Personal Answers Dictionary
     Dictionary<int, string> KalushiAnswers = new Dictionary<int, string>();
     Dictionary<int, string> BandileAnswers = new Dictionary<int, string>();
     Dictionary<int, string> JamesAnswers = new Dictionary<int, string>();
     Dictionary<int, string> ThandiAnswers = new Dictionary<int, string>();
     Dictionary<int, string> RoliAnswers = new Dictionary<int, string>();
 
+//General Answers Dictionary
     Dictionary<int, string> KalushiGeneralAnswers = new Dictionary<int, string>();
     Dictionary<int, string> BandileGeneralAnswers = new Dictionary<int, string>();
     Dictionary<int, string> JamesGeneralAnswers = new Dictionary<int, string>();
     Dictionary<int, string> ThandiGeneralAnswers = new Dictionary<int, string>();
     Dictionary<int, string> RoliGeneralAnswers = new Dictionary<int, string>();
 
-    Dictionary<string, string> ThoughtsAboutKalushi = new Dictionary<string, string>();
-    Dictionary<string, string> ThoughtsAboutBandile = new Dictionary<string, string>();
-    Dictionary<string, string> ThoughtsAboutJames = new Dictionary<string, string>();
-    Dictionary<string, string> ThoughtsAboutThandi = new Dictionary<string, string>();
-    Dictionary<string, string> ThoughtsAboutRoli = new Dictionary<string, string>();
+//Thoughts About Other People
+    Dictionary<int, string> ThoughtsAboutKalushi = new Dictionary<int, string>();
+    Dictionary<int, string> ThoughtsAboutBandile = new Dictionary<int, string>();
+    Dictionary<int, string> ThoughtsAboutJames = new Dictionary<int, string>();
+    Dictionary<int, string> ThoughtsAboutThandi = new Dictionary<int, string>();
+    Dictionary<int, string> ThoughtsAboutRoli = new Dictionary<int, string>();
 
     // Start is called before the first frame update
     void Start()
@@ -41,13 +46,11 @@ public class PlayerDialogue : MonoBehaviour
         NPCs.Add("Bandile");
 
         //general questions
-
         PlayerGeneralQuestions.Add(1, "What made you want to join Umkhonte we Sizwe?");
         PlayerGeneralQuestions.Add(2, "What do you think about "); //choice of names exclusing player/person talking to
         PlayerGeneralQuestions.Add(3, "Are you more of a pacifist or a warmonger?");
 
         //personal questions
-
         KalushiQuestions.Add(1, "Why was your sentence shortened?");
         KalushiQuestions.Add(2, "What's so great about Jazz?");
 
@@ -64,7 +67,6 @@ public class PlayerDialogue : MonoBehaviour
         RoliQuestions.Add(1, "How do you not see black officers as traitors?");
 
 //personal answers
-
         KalushiAnswers.Add(1, "How does that concern you? Mind your own business.");
         KalushiAnswers.Add(2, "He went to prison for stabbing a police officer.");
 
@@ -81,7 +83,6 @@ public class PlayerDialogue : MonoBehaviour
         RoliAnswers.Add(1, "My brother is a prison warden, they are just doing their jobs.");
 
         //General answers
-
         KalushiGeneralAnswers.Add(1, "When my brother was sent to prison, the police officers beat my brother to the ground. When the same thing happened to me a few years later, I knew I was going to fight to ensure that never would another innocent black be treated like that again.");
         KalushiGeneralAnswers.Add(2, "Warmonger");
 
@@ -94,29 +95,23 @@ public class PlayerDialogue : MonoBehaviour
         ThandiGeneralAnswers.Add(2, "I see how harshly the police treat those of us with black skin and I can't stand back and watch it happen anymore."); 
         ThandiGeneralAnswers.Add(1, "Warmonger");
 
-        RoliGeneralAnswers.Add(2, "I was help captive by a police officer whilst walking home day, he took his pants off and pissed on me. No one should be treated like this."); //check to see it this object has been interacted with
-        RoliGeneralAnswers.Add(1, "Warmonger");
+        RoliGeneralAnswers.Add(2, "I was held captive by a police officer whilst walking home day, he took his pants off and pissed on me. No one should be treated like this."); //check to see it this object has been interacted with
+        RoliGeneralAnswers.Add(1, "Warmonger"); //change
 
-        //thoughs about
+        //thoughts about
+        ThoughtsAboutKalushi.Add(1,"He received 10 years in prison, but got out 5 years early, no one knows how.");
+        ThoughtsAboutKalushi.Add(2,"He's an amazing jazz player, I really enjoy having him around.");
 
-        ThoughtsAboutKalushi.Add("positive","He received 10 years in prison, but got out 5 years early, no one knows how.");
-        ThoughtsAboutKalushi.Add("negative","He's an amazing jazz player, I really enjoy having him around.");
+        ThoughtsAboutBandile.Add(2,"I love his photos, you should ask him to take one of you.");
+        ThoughtsAboutBandile.Add(1,"He doesn't like music. Don't ever trust a man who doesn't like music.");
 
-        ThoughtsAboutBandile.Add("positive","I love his photos, you should ask him to take one of you.");
-        ThoughtsAboutBandile.Add("negative","He doesn't like music. Don't ever trust a man who doesn't like music.");
+        ThoughtsAboutJames.Add(1,"He has strong christian values and sings struggle songs with great fervour!");
+        ThoughtsAboutJames.Add(2,"It's a bit suspicious, how he only just joined the ANC");
 
-        ThoughtsAboutJames.Add("positive","He has strong christian values and sings struggle songs with great fervour!");
-        ThoughtsAboutJames.Add("negative","It's a bit suspicious, how he only just joined the ANC");
+        ThoughtsAboutThandi.Add(2,"She's a strong yet ruthless fighter, a true beneficiary to the cause");
+        ThoughtsAboutThandi.Add(1,"I'm not sure how loyal she is to the cause, considering that she is not South African.");
 
-        ThoughtsAboutThandi.Add("positive","She's a strong yet ruthless fighter, a true beneficiary to the cause");
-        ThoughtsAboutThandi.Add("negative","I'm not sure how loyal she is to the cause, considering that she is not South African.");
-
-        ThoughtsAboutRoli.Add("positive","He's a skilled boxer, and I heard that he met with Robert Subukwe in person for legal advice.");
-        ThoughtsAboutRoli.Add("negative","He's too sympathetic towards black traitors who guard the prisons.");
-    }
-
-    void Update()
-    {
-        
+        ThoughtsAboutRoli.Add(2,"He's a skilled boxer, and I heard that he met with Robert Subukwe in person for legal advice.");
+        ThoughtsAboutRoli.Add(1,"He's too sympathetic towards black traitors who guard the prisons.");
     }
 }
