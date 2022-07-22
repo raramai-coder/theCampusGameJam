eacc5b7667ff80041520a26047c699773fd8cb6c
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-07-23 00:24:33 +0200

    added character scriptable objects

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CreateCharacter", order = 1)]
+public class Character : ScriptableObject
 {
 
-    private string name;
-    private bool decoy;
+    [SerializeField] private string name, personalQuestion1, personalQuestion2, answer1, answer2, answer3, answer4, thought1, thought2;
+    [SerializeField] public bool decoy = false;
     private int calls;
     private int numOfTimeAskedAbout;
-    private Dictionary<int, string> questions;
-    private Dictionary<int, string> answers;
-    private Dictionary<int, string> thoughts;
+    private Dictionary<int, string> questions = new Dictionary<int, string>();
+    private Dictionary<int, string> answers = new Dictionary<int, string>();
+    private Dictionary<int, string> thoughts = new Dictionary<int, string>();
     private List<int> availableQuestions;
 
-    public Character(string name,bool decoy,string personalQuestion1, string personalQuestion2, string answer1, string answer2, string answer3, string answer4, string thought1, string thought2)
+    private void OnEnable()
+    {
+        SetQuestions(personalQuestion1, personalQuestion2);
+        SetAnswers(answer1, answer2, answer3, answer4);
+        SetThoughts(thought1, thought2);
+        calls = 0;
+        numOfTimeAskedAbout = 0; 
+        availableQuestions = new List<int>() { 1, 2, 3, 4, 5 };
+    }
+
+
+    /*public Character(string name,bool decoy,string personalQuestion1, string personalQuestion2, string answer1, string answer2, string answer3, string answer4, string thought1, string thought2)
     {
         this.name = name;
         this.decoy = decoy;
@@ -22,7 +34,7 @@ public class Character
         SetQuestions(personalQuestion1,personalQuestion2);
         SetAnswers(answer1,answer2,answer3,answer4);
         SetThoughts(thought1,thought2);
-    } 
+    } */
 
     private void SetThoughts(string thought1,string thought2)
     {
@@ -32,10 +44,10 @@ public class Character
 
     private void SetAnswers(string answer1, string answer2, string answer3, string answer4)
     {
-        questions.Add(1, answer1);
-        questions.Add(2, answer2);
-        questions.Add(3, answer3);
-        questions.Add(4, answer4);
+        answers.Add(1, answer1);
+        answers.Add(2, answer2);
+        answers.Add(3, answer3);
+        answers.Add(4, answer4);
     }
 
     private void SetQuestions(string question4,string question5)
@@ -47,28 +59,5 @@ public class Character
         questions.Add(5, "Ask about another person.");
     }
     
-    private void Call()
-    {
-
-    }
-
-    private void DisplayQuestions()
-    {
-
-    }
-
-    private void AskQuestions()
-    {
-
-    }
-
-    private void AskAbout()
-    {
-
-    }
-
-    private void Report()
-    {
-
-    }
+  
 }
